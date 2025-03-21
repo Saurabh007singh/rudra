c15bc953e4711a1a48d310ed58110df5e300b676
Saurabh007singh <132544840+Saurabh007singh@users.noreply.github.com>
2025-03-21 18:23:33 +0530

    Update payviaupi.jsx

--- client/src/components/payment/payviaupi.jsx
+++ client/src/components/payment/payviaupi.jsx
@@ -5,15 +5,13 @@ import { useNavigate } from "react-router-dom";
 
 export function PayUpi() {
   const { user, isLoading } = useSelector((state) => state.auth);
-  const {addressList}=useSelector(state=>state.address)
-const dispatch=useDispatch()
-const navigate=useNavigate()
- 
-
+  const { addressList } = useSelector((state) => state.address);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const { cartItems } = useSelector((state) => state.shopCart);
 
- const totalCartAmount =
+  const totalCartAmount =
     cartItems && cartItems.length > 0
       ? cartItems.reduce(
           (sum, currentItem) =>
@@ -26,71 +24,75 @@ const navigate=useNavigate()
         )
       : 0;
 
-
   const ordersData = {
     userId: user.id,
     cartItems: cartItems,
-    address:addressList[0],
-    totalAmount:totalCartAmount,
-    orderDate:new Date(),
-    paymentMethod:"UPI"
+    address: addressList[0],
+    totalAmount: totalCartAmount,
+    orderDate: new Date(),
+    paymentMethod: "UPI",
   };
 
-
-function handlePayment(){
-  dispatch(createOrder(ordersData)).then(()=>{
-    dispatch(deleteCart({userId:user?.id}));
-    
-  }).then(()=>{
-      navigate("/confirmation");
-      
-    })
-}
- 
+  function handlePayment() {
+    dispatch(createOrder(ordersData))
+      .then(() => {
+        dispatch(deleteCart({ userId: user?.id }));
+      })
+      .then(() => {
+        navigate("/confirmation");
+      });
+  }
 
   return (
     <>
-    {totalCartAmount !==0 ?<div className="flex w-full justify-center items-center ">
-      <div className="h-[600px] w-[500px] border my-5 flex items-center flex-col gap-2">
-        <span className="mt-2 text-2xl text-[#5E7BA5] ">PAY VIA UPI QR</span>
-        <span>Your Total</span>
-        <span>₹{totalCartAmount}</span>
-        <div className="w-[400px] h-[400px] bg-red-300">df</div>
-        <span>
-    
-          1.Pay The amount here on this Qr Code <br />
-          2.After Successful Payment Click on the button below
-          <br />
-          3.And send a screeshot on our Whats app +919999999999
-          <br />
-          4.Once Confirmed We will proceed to ship your order
-        </span>
-        <div className="flex w-full flex-row">
-          <button  onClick={handlePayment} className="text-md w-full h-12 bg-[#786B4A] text-white">
-            Payment Done
-          </button>
-        </div>
-      </div>
-    </div>:<div className="bg-gray-100 flex items-center justify-center h-screen">
-            <div className="bg-white p-12 rounded-lg shadow-lg text-center max-w-md w-full">
-                <div className="text-6xl text-orange-500 mb-4">
-                    🛒
-                </div>
-                <h1 className="text-3xl font-bold text-gray-800 mb-4">
-                    Your Cart is Empty
-                </h1>
-                <p className="text-lg text-gray-600 mb-8">
-                    It looks like you haven't added anything to your cart yet. Let's change that!
-                </p>
-                <button 
-                    className="bg-orange-500 text-white py-2 px-6 rounded-lg text-lg font-semibold hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-400"
-                    onClick={() => window.location.href = '/shop/home'} 
-                >
-                    Start Shopping
-                </button>
+      {totalCartAmount !== 0 ? (
+        <div className=" flex justify-center w-full "><div className="flex w-[80%] justify-center  items-center  ">
+          <div className="h-[600px] w-[500px]  border my-5 flex items-center flex-col gap-2 ">
+            <span className="mt-2 text-2xl text-[#5E7BA5] ">
+              PAY VIA UPI QR
+            </span>
+            <span>Your Total</span>
+            <span>₹{totalCartAmount}</span>
+            <div className="w-[400px] h-[400px] bg-red-300 p-3">df</div>
+            <span>
+              1.Pay The amount here on this Qr Code <br />
+              2.After Successful Payment Click on the button below
+              <br />
+              3.And send a screeshot on our Whats app +919999999999
+              <br />
+              4.Once Confirmed We will proceed to ship your order
+            </span>
+            <div className="flex w-full flex-row">
+              <button
+                onClick={handlePayment}
+                className="text-md w-full h-12 bg-[#786B4A] text-white"
+              >
+                Payment Done
+              </button>
             </div>
-        </div>}
+          </div>
+        </div></div>
+        
+      ) : (
+        <div className="bg-gray-100 flex items-center justify-center h-screen">
+          <div className="bg-white p-12 rounded-lg shadow-lg text-center max-w-md w-full">
+            <div className="text-6xl text-orange-500 mb-4">🛒</div>
+            <h1 className="text-3xl font-bold text-gray-800 mb-4">
+              Your Cart is Empty
+            </h1>
+            <p className="text-lg text-gray-600 mb-8">
+              It looks like you haven't added anything to your cart yet. Let's
+              change that!
+            </p>
+            <button
+              className="bg-orange-500 text-white py-2 px-6 rounded-lg text-lg font-semibold hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-400"
+              onClick={() => (window.location.href = "/shop/home")}
+            >
+              Start Shopping
+            </button>
+          </div>
+        </div>
+      )}
     </>
-    
   );
 }
