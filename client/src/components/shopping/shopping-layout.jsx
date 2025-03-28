ad11e8c97bc4f62e29f3a9425c0892a151374695
Saurabh007singh <132544840+Saurabh007singh@users.noreply.github.com>
2025-03-28 18:01:46 +0530

    changes added whats app

--- client/src/components/shopping/shopping-layout.jsx
+++ client/src/components/shopping/shopping-layout.jsx
@@ -8,7 +8,7 @@ import { fetchAllProducts } from "@/store/admin/product-slice";
 import { fetchCartItems } from "@/store/shop/cart-slice/cart-slice";
 import { Footer } from "./shopping-footer";
 import { fetchWhishList } from "@/store/shop/whish-list-slice";
-import { FaWindows } from "react-icons/fa";
+import { FaPhone, FaWhatsapp, FaWindows } from "react-icons/fa";
 import { getAllAddress } from "@/store/shop/address-slice";
 
 export const ShoppingLayout = () => {
@@ -39,6 +39,30 @@ export const ShoppingLayout = () => {
         </main>
       </div>
       <Footer></Footer>
+
+      <div className="fixed bottom-6 left-1/2 transform -translate-x-1/2  md:left-auto md:right-2 z-50 ">
+      <div className="flex gap-5"><a
+    href="https://wa.me/+919999999999"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="text-green-500 hover:text-green-600 transition-transform transform hover:scale-110"
+    aria-label="WhatsApp"
+  >
+    <FaWhatsapp size={40} />
+  </a>
+
+  {/* Phone Icon */}
+  <a
+    href="tel:+91999999999"
+    className="text-blue-500 lg:hidden md:hidden xl:hidden 2xl:hidden hover:text-blue-600 transition-transform transform hover:scale-110"
+    aria-label="Call us"
+  >
+    <FaPhone size={40} className="transform scale-x-[-1]" />
+  </a></div>
+  {/* WhatsApp Icon */}
+  
+</div>
+
     </div>
   );
 };
